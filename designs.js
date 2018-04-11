b279c3baaccd4b01df074c1baad5177d365ac86f
Manager By Day Coder By Night <moniquejhampton@yahoo.com>
2018-04-11 17:17:12 -0400

    Added grid, preview, clear, and download function.

--- designs.js
+++ designs.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  let artwork = $("#pixelCanvas");
+  let buildCanvas = "";
   //Sets the default color for the color picker
   $("#colorPicker").val("#c6a530");
   let isClicked = false;
@@ -19,9 +21,9 @@ $(document).ready(function() {
   //Single Click Event
   $("#pixelCanvas")
     .on("click", "td", function(e) {
-    gridCol = $("#colorPicker").val();
-    $(this).css("background-color", gridCol);
-    isClicked = false;
+      gridCol = $("#colorPicker").val();
+      $(this).css("background-color", gridCol);
+      isClicked = false;
     })
     .on("dblclick", "td", function(e) {
       $(e.target).css("background-color", "white");
@@ -34,18 +36,17 @@ $(document).ready(function() {
     .on("mouseover", "td", function(e) {
       //Check: Is the mouse clicked?
       if (isClicked) {
-    gridCol = $("#colorPicker").val();
-    $(this).css("background-color", gridCol);
+        gridCol = $("#colorPicker").val();
+        $(this).css("background-color", gridCol);
       }
     });
-  
+
   function makeGrid() {
     resetGrid(); //Resets the grid and removes old pixel art
     //Creates table row parents
     for (let rows = 0; rows < rowCount; rows++) {
       let gridVal = $("<tr></tr>");
       $("#pixelCanvas").append('<tr class="gridRow"></tr>');
-      //alert(gridVal);
     }
     //Creates and append table data (columns) into parent table rows
     for (let cols = 0; cols < colCount; cols++) {
@@ -60,6 +61,43 @@ $(document).ready(function() {
     $(".gridRow").remove(); //removes old grid
   }
 
+  // Remove/add grid border
+  $("#removeGrid").on("click", function() {
+    $("td").toggleClass("removeGrid");
+    $("tr").toggleClass("removeGrid");
+  });
+
+  // Clear all color
+  $("#clearGrid").on("click", function() {
+    $("#pixelCanvas td").css("background-color", "white");
+  });
+
+  //Preview your pixel art
+  $("#preview").on("click", function() {
+    $("#previewScreen").empty();
+    $("#downloadArt").removeClass("disabled");
+    html2canvas(artwork, {
+      onrendered: function(canvas) {
+        $("#previewScreen").append(canvas);
+        $("#previewScreen")
+          .find("canvas")
+          .addClass("canvas");
+        buildCanvas = canvas;
+      }
+    });
+    $("#previewModal").modal();
+  });
+
+  //Saves Pixel Canvas as a .png
+  $("#downloadArt").on("click", function() {
+    let image = buildCanvas.toDataURL("image/png");
+
+    let newData = image.replace(
+      /^data:image\/png/,
+      "data:application/octet-stream"
+    );
+    $("#downloadArt")
+      .attr("download", "PixelArt.png")
+      .attr("href", newData);
+  });
 });
-
-
